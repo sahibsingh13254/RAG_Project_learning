31109f8220e576e3bbc9b8ba7d6f0b0813b03fdb
sahibsingh13254 <sahibs98765432@gmail.com>
2026-07-12 22:52:46 +0530

    Document loaders -textfile

---
+++ document_loaders/test.py
@@ -0,0 +1,7 @@
+from langchain_community.document_loaders import TextLoader
+
+data = TextLoader("document_loaders/notes.txt")
+
+docs = data.load()
+
+print(docs)
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
+from langchain_community.document_loaders import TextLoader
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 model = ChatMistralAI(model = "mistral-small-2603")
 
-result = model.invoke("hello")
+data = TextLoader("document_loaders/notes.txt")
+docs = data.load()
+
+template = ChatPromptTemplate.from_messages(
+    [("system" ,"You are AI that summarize the text"), 
+     ("human", "{data}")]
+)
+prompt = template.format_messages(data = docs[0].page_content)
+
+result = model.invoke(prompt)
 
 print(result.content)
