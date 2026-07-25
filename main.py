39ab32be87335765f0c8d90eb54aecd25265571c
sahibsingh13254 <sahibs98765432@gmail.com>
2026-07-25 23:52:23 +0530

    Added vector_store

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 from dotenv import load_dotenv
-from langchain_mistralai import ChatMistralAI
-from langchain_community.document_loaders import TextLoader
+from langchain_mistralai import MistralAIEmbeddings
+from langchain_community.vectorstores import Chroma
 from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
-model = ChatMistralAI(model = "mistral-small-2603")
 
-data = TextLoader("document_loaders/notes.txt")
-docs = data.load()
-
-template = ChatPromptTemplate.from_messages(
-    [("system" ,"You are AI that summarize the text"), 
-     ("human", "{data}")]
-)
-prompt = template.format_messages(data = docs[0].page_content)
-
-result = model.invoke(prompt)
-
-print(result.content)
+embedding_model = MistralAIEmbeddings
